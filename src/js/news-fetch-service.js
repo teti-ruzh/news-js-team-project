ece908d0bc58906531ce367385a9276e3ac94090
LiudmylaKozytska <ludaanishchenko0512@gmail.com>
2023-03-23 13:26:28 +0100

    API

--- src/js/news-fetch-service.js
+++ src/js/news-fetch-service.js
@@ -1,1 +1,49 @@
 // Creating fetch from API
+
+// query змінна, значення якої нам прийде з пошуку у хедері
+let query = '';
+
+const API_KEY = 'api-key=nb4kIc3A28NYQPkulI6xtxUAkPze1R9u';
+
+const MOST_POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?${API_KEY}`;
+const CATEGORY_URL = `https://api.nytimes.com/svc/news/v3/content/all/all.json?${API_KEY}`;
+const SEARCH_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&${API_KEY}`;
+
+export async function fetchApiPopular() {
+  try {
+    const response = await fetch(MOST_POPULAR_URL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchApiCategory() {
+  try {
+    const response = await fetch(CATEGORY_URL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchApiSearchQuery() {
+  try {
+    const response = await fetch(SEARCH_URL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
