f1792223b09f36157a1dc12bb60ac0289c4fd8ed
LiudmylaKozytska <ludaanishchenko0512@gmail.com>
2023-03-23 13:26:07 +0100

    API

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,19 +1,4 @@
-const API_KEY = 'api-key=nb4kIc3A28NYQPkulI6xtxUAkPze1R9u';
-const MOST_POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?${API_KEY}`;
+import { fetchApiCategory } from './news-fetch-service';
+const btn2 = document.querySelector('#btn2');
 
-const btn = document.querySelector('#btn-1');
-btn.addEventListener('click', fetchApi);
-
-function foo() {
-  console.log('hello');
-}
-foo();
-
-async function fetchApi() {
-  await fetch(MOST_POPULAR_URL).then(response => {
-    if (response.ok) {
-      console.log(response.json());
-      return response.json();
-    }
-  });
-}
+btn2.addEventListener('click', fetchApiCategory);
