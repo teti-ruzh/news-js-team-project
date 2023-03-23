89d76abc785683bfe244d881ecf69401a8131460
LiudmylaKozytska <ludaanishchenko0512@gmail.com>
2023-03-23 23:58:45 +0100

    done-dinamic-btn
    add names of categories, rendering dinamic buttons
    IMPORTANT approve my changes in API fetch file

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,4 +1,31 @@
-// import { fetchApiCategory } from './news-fetch-service';
-// const btn2 = document.querySelector('#btn2');
+import { fetchApiCategory } from './news-fetch-service';
 
-// btn2.addEventListener('click', fetchApiCategory);
+const othersBtn = document.querySelector('#others-btn');
+const categoryList = document.querySelector('#category-btn');
+
+categoryList.addEventListener('click', getCategory);
+
+async function getCategory() {
+  const categories = await fetchApiCategory().then(categories => {
+    return categories.map(category => category.display_name);
+  });
+  return categories;
+}
+
+async function renderCategoryButtons() {
+  const sections = await getCategory();
+  const sortSections = sections.slice(0, 6);
+
+  let btnRender = '';
+  for (let i = 0; i < sortSections.length; i++) {
+    const section = sortSections[i];
+    btnRender += `<li class="category__item">
+      <button class="category__btn" type="button">
+          ${section}
+      </button>
+    </li>`;
+  }
+  categoryList.innerHTML = btnRender;
+}
+
+renderCategoryButtons();
