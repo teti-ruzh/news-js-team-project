0d67c2dc029c51da78dafbc2b5d64b80c49e88bf
MisuykMaryna <misyuk.marina@gmail.com>
2023-03-28 18:22:09 +0200

    input-read

--- src/js/error-image.js
+++ src/js/error-image.js
@@ -1,11 +1,12 @@
 
-// import { fetchApiSearchQuery } from './render-list-of-readed';
+// import { renderFavourite } from './favourite.js';
 
 
-const STORAGE_KEY = 'add-read-more';
+
 const errorImage = document.querySelector(".errorImage");
 const search = document.querySelector(".search");
 const searchInput = document.querySelector(".search__input");
+const myData = JSON.parse(localStorage.getItem('favorite'));
 
 searchInput.addEventListener("submit", selectionNews);
 
@@ -13,16 +14,36 @@ searchInput.addEventListener("submit", selectionNews);
     e.preventDefault();
    const query = e.currentTarget.elements.query.value.trim();
      if (!Boolean(query)) {
-    // Дів в який внесли розмітку 
-        //  Функція в якій знаходиться уся розмітка
+    // Дів в який внесли розмітку
+      //  renderFavourite(myData);
+       errorImage.classList.add('visually-hidden');
         return;
      }
      
+  let markup = '';
+  let foundNews = [];
 
-
-
- if (query.length === 0) {
+markup = myData.map(
+    ({ id, url, foto, section, title, newsAbstruct, newDataFormat }) => {
+      if (title.toLowerCase().includes(query.toLowerCase())) {
+        foundNews.push({
+          id: `${id}`,
+          url: `${url}`,
+          foto: `${foto}`,
+          section: `${section}`,
+          title: `${title}`,
+           newsAbstruct: `${ newsAbstruct}`,
+          newDataFormat: `${newDataFormat}`,
+        });
+      }
+    }
+  );
+  //  renderFavourite(myData);
+  if (foundNews.length === 0) {
     errorImage.classList.remove('visually-hidden');
   }
-
 }
+   
+   
+   
+
