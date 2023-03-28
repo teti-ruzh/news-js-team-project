644fd59cb6d9778c3357f8384aa2c64ed552ddbf
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-03-28 17:54:19 +0300

    Change functions for read page

--- src/js/add-to-read.js
+++ src/js/add-to-read.js
@@ -12,16 +12,31 @@ populateNewsData();
 function onReadMoreBtnClick(event) {
   const readMoreBtn = event.target.closest('.news__link');
   if (readMoreBtn) {
-    const newsItem = readMoreBtn.closest('.news__item').outerHTML;
+    // const newsItem = readMoreBtn.closest('.news__item').outerHTML;
+    const newsItem = readMoreBtn.closest('.news__item');
+    const title = newsItem.querySelector('.news__title').textContent;
+    const section = newsItem.querySelector('.news__category-text').textContent;
+    const foto = newsItem.querySelector('.news__foto').getAttribute('src');
+    const abstruct = newsItem.querySelector('.news__abstruct').textContent;
+    const publishDate = newsItem.querySelector('.news__data').textContent;
+    const url = newsItem.querySelector('.news__link').getAttribute('href');
+
     const clickDate = new Date();
+    const clickID = clickDate.getTime();
     const day = clickDate.getDate().toString().padStart(2,'0');
     const month = (clickDate.getMonth()+1).toString().padStart(2,'0');
     const date = `${day}/${month}/${clickDate.getFullYear()}`;
     clickDate.getFullYear();
-    let newsObj = {};
-    newsObj.id = clickDate.getTime();
-    newsObj.date = date;
-    newsObj.card = `${newsItem}`;
+    
+    // let newsObj = {};
+    // newsObj.id = clickDate.getTime();
+    // newsObj.date = date;
+    // newsObj.card = `${newsItem}`;
+    
+
+    const newsObj = { url, foto, section, title, abstruct, publishDate, clickID, date };
+
+    console.log(newsObj);
     readedNewsData.push(newsObj);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(readedNewsData));
   }
